ea15cd84235f563d65862d3898d01d2e8b0391a0
Carlos Alvarado <carlosalvaradom@icloud.com>
2023-03-17 07:43:45 -0500

    First Commit

---
+++ src/test/java/co/edu/ucatolica/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucatolica.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
